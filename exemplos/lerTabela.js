083400946baf4cf41d5f4d7cc7a34eee6e6af626
Marcelo Rozgrin Marques <marcelo.rozgrin@gmail.com>
2024-10-07 13:20:29 -0300

    inicio

---
+++ exemplos/lerTabela.js
@@ -0,0 +1,19 @@
+
+var axios = require('axios');
+
+const tabela = 'igpm'
+const apikey = 'sua-api-key'
+
+var params = new URLSearchParams();
+params.append('tabela', tabela);
+params.append('apikey', apikey)
+
+const url = 'https://client-api.debit.com.br/atualiza-v1/lerTabela'
+
+
+axios.post(url, params).
+then(function (resposta) {
+    console.log(resposta.data)
+}).catch(function (error) {
+    console.log(error)
+})
