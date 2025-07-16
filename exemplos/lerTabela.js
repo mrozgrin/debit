44af1dfc8b018ca22ae37031fcf9864255a2df56
Marcelo Rozgrin Marques <marcelo.rozgrin@gmail.com>
2025-07-16 15:03:16 -0300

    xxxxx

--- exemplos/lerTabela.js
+++ exemplos/lerTabela.js
@@ -1,18 +1,19 @@
 const axios = require('axios');
 
-const tabela = 'igpm'
-const apikey = 'sua-api-key'
-
-let params = new URLSearchParams();
-params.append('tabela', tabela);
-params.append('apikey', apikey)
+let params = {
+    // tabela: 'igpm',
+    tabela: 'tj-sp-lei-14905',     // escolha a tabela. Veja a lista de tabelas disponíveis: https://client-api.debit.com.br//atualiza-v1/listaTabelas
+    dataAtualizacao: '01/04/2025', // Se o campo permiteSelic for true, informe a data de atualização da tabela
+    dataInicioSelic: '01/01/2024', // Se o campo permiteSelic for true, informe a data de inicial da Selic 
+    apikey: 'sua-api-key',
+}
 
 const url = 'https://client-api.debit.com.br/atualiza-v1/lerTabela'
 
-
-axios.post(url, params).
-then(function (resposta) {
-    console.log(resposta.data)
-}).catch(function (error) {
-    console.log(error)
-})
+axios.post(url, params)
+    .then(function (resposta) {
+        console.log(resposta.data)
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
