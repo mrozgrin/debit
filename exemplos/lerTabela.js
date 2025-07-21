7599ab44181ee10a4d0e3d5e2c6175da06d5a7b0
Marcelo Rozgrin Marques <marcelo.rozgrin@gmail.com>
2025-07-21 14:42:51 -0300

    xxx

--- exemplos/lerTabela.js
+++ exemplos/lerTabela.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const axios = require('axios');
 
 let params = {
-    tabela: 'tj-sp-lei-14905',     // escolha a tabela. Veja a lista de tabelas disponíveis: https://client-api.debit.com.br//atualiza-v1/listaTabelas
+    tabela: 'igpm',     // escolha a tabela. Veja a lista de tabelas disponíveis: https://client-api.debit.com.br//atualiza-v1/listaTabelas
     dataAtualizacao: '01/04/2025', // Se o campo permiteSelic for true, informe a data de atualização da tabela
     dataInicioSelic: '01/01/2024', // Se o campo permiteSelic for true, informe a data de inicial da Selic 
     apikey: process.env.API_KEY,
 }
 
-const url = 'https://client-api.debit.com.br/atualiza-v1/lerTabela'
+// const url = 'https://client-api.debit.com.br/atualiza-v1/lerTabela'
+const url = 'http://localhost:3102/atualiza-v1/lerTabela'
 
 axios.post(url, params)
     .then(function (resposta) {
